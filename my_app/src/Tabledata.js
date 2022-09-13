477be5083cfea1b562bee83b29de056a34374dd8
Shashank Gupta <shashank.gupta@ithours.com>
2022-09-13 12:11:39 +0530

    delete function is complete

--- my_app/src/Tabledata.js
+++ my_app/src/Tabledata.js
@@ -1,43 +1,54 @@
-import React from 'react'
-import {useState} from 'react';
-import './Tabledata.css'
-import Table1 from "./Table1"
-import jsondata from './data.json'
+import React from "react";
+import { useState } from "react";
+import "./Tabledata.css";
+import Table1 from "./Table1";
 
 const Tabledata = () => {
-    const [studentData, setStudentData] = useState(jsondata)
-    console.log("studentData",studentData);
-    console.log("data",jsondata);
-    const tableRow = studentData.map((info)=>{
-        return (
-            <tr>
-                <td>{info.name}</td>
-                <td>{info.age}</td>
-                <td><button>delete</button></td>
-            </tr>
-        )
-    });
-        const addRow = (data)=>{
-            const updatedStudentData = [...studentData];
-            updatedStudentData.push(data)
-            console.log(data[2]);
-            setStudentData(updatedStudentData);
-        }
+  const [studentData, setStudentData] = useState([]);
+
+  console.log([...studentData]);
+
+  const deletestudent = (i) => {
+    let delData =  [...studentData];
+    delData = delData.filter((_,idx)=>i!==idx)
+    setStudentData(delData);
+  };
+
+  console.log("studentData", studentData[0]);
+
+  const tableRow = studentData.map((info, i) => {
+    return (
+      <tr key={i}>
+        <td>{info.name}</td>
+        <td>{info.age}</td>
+        <td>
+          <button onClick={(e) => deletestudent(i)}>delete</button>
+        </td>
+      </tr>
+    );
+  });
+  const addRow = (data) => {
+    const updatedStudentData = [...studentData];
+    updatedStudentData.push(data);
+
+    setStudentData(updatedStudentData);
+  };
+
   return (
     <div>
-        <Table1 func={addRow}/>
-        <table id="table24">
-            <thead >
-                <tr>
-                    <th>Name</th>
-                    <th>Age</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>{tableRow}</tbody>
-        </table>
+      <Table1 func={addRow} />
+      <table id="table24">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>{tableRow}</tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Tabledata
+export default Tabledata;
